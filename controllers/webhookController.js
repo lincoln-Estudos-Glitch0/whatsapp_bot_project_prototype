c2ff8a0d6e877e0eb89f41b6d15bc6f55e7c5018
lincoln-Estudos-Glitch0 <facebook.estudos.24.46919.dev@gmail.com>
2024-07-03 22:25:44 -0300

    increasing more code

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -1,5 +1,8 @@
 import { Router } from "express"
 import {strategyClass } from '../components/webhooksStrategy/webhooksStrategyClass.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 let route = Router()
 
@@ -16,7 +19,20 @@ route.post('/webhook', (req, res) => {
     res.sendStatus(200).end()
 })
 
+route.get('/webhook', (req, res) => {
 
+    let verify = req.query['hub.verify_token']
+    let mode = req.query['hub.mode']
+    let test = req.query['hub.challenge']
+
+    if( mode == 'subscribe' && verify == process.env.WEBHOOK_VERIFY_TOKEN){
+
+        res.status(200).send(test)
+    }
+    else {
+        res.sendStatus(403)
+    }
+})
 
 
 
